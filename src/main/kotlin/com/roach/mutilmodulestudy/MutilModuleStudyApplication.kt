4f92852dee29a258cf1df1930329cad07c42c3c1
roach <tmdgusya@naver.com>
2021-11-30 22:35:33 +0900

    init project

---
+++ src/main/kotlin/com/roach/mutilmodulestudy/MutilModuleStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.roach.mutilmodulestudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MutilModuleStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MutilModuleStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/roach/mutilmodulestudy/MutilModuleStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roach.mutilmodulestudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MutilModuleStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
